5b40582809d28b5d73484b242cf29353cee64208
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-01-03 22:50:46 +0300

    init + basic usage redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { userSlice } from "./store/reducers/UserSlice";
+import { useAppSelector, useAppDispatch } from "./hooks/redux";
 
 function App() {
+  const { count } = useAppSelector((state) => state.userReducer);
+
+  const { increment } = userSlice.actions;
+  const dispatch = useAppDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment(10))}>INCREMENT</button>
     </div>
   );
 }
